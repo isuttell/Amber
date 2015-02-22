15f474e9c1cbf0924a45016e46a033a7d6cda757
Isaac Suttell <isaac@isaacsuttell.com>
2015-02-22 12:00:48 -0800

    added function to check if image is loaded

--- src/utilities.js
+++ src/utilities.js
@@ -225,6 +225,33 @@
       return (name || 'id') + (++_uniqueId);
     };
 
+
+    /**
+     * Check to see if an image is already loaded
+     * http://stackoverflow.com/a/1977898/2489839
+     *
+     * @param  {Object}  img Image Object
+     * @return {Boolean}
+     */
+    var isImageLoaded = function(img) {
+      // During the onload event, IE correctly identifies any images that
+      // weren’t downloaded as not complete. Others should too. Gecko-based
+      // browsers act like NS4 in that they report this incorrectly.
+      if (!img.complete) {
+        return false;
+      }
+
+      // However, they do have two very useful properties: naturalWidth and
+      // naturalHeight. These give the true size of the image. If it failed
+      // to load, either of these should be zero.
+      if (typeof img.naturalWidth !== 'undefined' && img.naturalWidth === 0) {
+        return false;
+      }
+
+      // No other way of checking: assume it’s ok.
+      return true;
+    };
+
     return {
       uniqueId: uniqueId,
       has: has,
@@ -236,7 +263,8 @@
       isIterable: isIterable,
       runOnce: runOnce,
       results: results,
-      assign: assign
+      assign: assign,
+      isImageLoaded: isImageLoaded
     };
 
   });
