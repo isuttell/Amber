d34811ded06101a039a8e994ef78ea86fa815f02
Isaac Suttell <isaac@isaacsuttell.com>
2015-02-22 12:00:28 -0800

    refactored run to be less complex and added comments

--- src/run.js
+++ src/run.js
@@ -5,6 +5,74 @@
    */
 
   /**
+   * Gets a list of all the modules names that have already been found. If an
+   * module requires something that can't be found in this list then we can't
+   * resolve it.
+   *
+   * @param     {Array}     definitions    Modules to be applied
+   * @param     {Object}    modules        Modules already applied
+   *
+   * @return    {Array}
+   */
+  var getNames = function(definitions, modules) {
+    var names = [];
+    var i = definitions.length;
+    while(--i >= 0) {
+      names.push(definitions[i].name);
+    }
+    // And add any existing module names
+    for(i in modules) {
+      names.push(i);
+    }
+    return names;
+  };
+
+  /**
+   * The module factory is initialized with a list of names and modules. Then
+   * the create method takes a module definition and looks to see if the
+   * definitions dependencies are ready yet. If they are we pass the completed
+   * definition or falsey
+   *
+   * @private
+   * @param    {Array}     names      Used to check to see if a module exists at all
+   * @param    {Object}    modules    Completed modules
+   */
+  var ModuleFactory = function(names, modules) {
+
+    /**
+     * Loop through each definition's arguments to check to see if they have
+     * already been applied or not. If not then we add it back into the loop
+     * and try again later
+     *
+     * @param     {Object}    definition    Object pulled from `Amber.define`
+     *
+     * @return    {Object|Boolean}          Either completed definition or falsey
+     */
+    this.create = function(definition) {
+      var add = true;
+
+      var d = definition.deps ? definition.deps.length : 0;
+      while(--d >= 0) {
+        if(names.indexOf(definition.deps[d]) === -1) {
+          throw new Error("Unable to find module: " + definition.deps[d]);
+        } else if(typeof modules[definition.deps[d]] !== 'undefined') {
+          definition.deps[d] = modules[definition.deps[d]];
+        } else if(typeof window[definition.deps[d]] !== 'undefined') {
+          definition.deps[d] = window[definition.deps[d]];
+        } else {
+          add = false;
+        }
+      }
+
+      if(add){
+        return definition;
+      } else {
+        return false;
+      }
+    };
+  };
+
+  /**
    * Loads any unloaded modules into the framework and acts as a starting point
    * for the application
    *
@@ -13,70 +81,62 @@
    * @param     {Function}    callback    Optional callback
    */
   Amber.run = function(deps, callback) {
-    if(!Amber._modules) {
-      throw new Error("No new modules to load");
-    }
+    // Optional callback
     if(typeof deps === 'function') {
       callback = deps;
       deps = [];
     }
+
+    // Save a copy of any new definitions and remove them from the queue
     var definitions = Amber._modules;
     delete Amber._modules;
 
+    // Ensure the module directory exists
     Amber.$$modules = Amber.$$modules || {};
     var modules = Amber.$$modules;
 
-    var names = [];
-    var i = definitions.length;
+    // Save a list of all defined modules so we can check to see if new modules
+    // are requiring modules we can actually find
+    var names = getNames(definitions, modules);
 
     // Put a check in here so we don't loop forever trying to find something
     // that does not exist
-    var maxIterations = 1;
+    var maxIterations = names.length * names.length;
 
-    // Get a list of new module names
-    while(--i >= 0) {
-      names.push(definitions[i].name);
-      maxIterations += definitions[i].deps.length + 1;
-    }
-    // And add any existing module names
-    for(i in Amber.$$modules) {
-      names.push(i);
-      maxIterations += 1;
-    }
+    // Start the factory which setups up each definition so we can apply it
+    var factory = new ModuleFactory(names, modules);
 
-    var d;
-    var add;
-    i = -1;
+    // Loop through each new definition
+    var i = -1;
     while(++i < definitions.length) {
-      add = true;
-      d = definitions[i].deps ? definitions[i].deps.length : 0;
-      while(--d >= 0) {
-        if(names.indexOf(definitions[i].deps[d]) === -1) {
-          throw new Error("Unable to find module: " + definitions[i].deps[d]);
-        } else if(typeof modules[definitions[i].deps[d]] !== 'undefined') {
-          definitions[i].deps[d] = modules[definitions[i].deps[d]];
-        } else if(typeof window[definitions[i].deps[d]] !== 'undefined') {
-          definitions[i].deps[d] = window[definitions[i].deps[d]];
-        } else {
-          add = false;
-        }
-      }
-      if(add && typeof definitions[i].extend !== 'undefined' && modules[definitions[i].extend]) {
-        modules[definitions[i].name] = modules[definitions[i].extend].extend(definitions[i].fn.apply(Amber, definitions[i].deps));
-      } else if(add && typeof definitions[i].extend === 'undefined') {
-        modules[definitions[i].name] = definitions[i].fn.apply(Amber, definitions[i].deps);
+      // Check to see if the definition is ready to be applied
+      var definition = factory.create(definitions[i]);
+
+      if(definition && typeof definition.extend !== 'undefined' && modules[definition.extend]) {
+        // If the module is uses the `extend` module and defines then we apply it here
+        modules[definition.name] = modules[definition.extend].extend(definition.fn.apply(Amber, definition.deps));
+      } else if(definition && typeof definition.extend === 'undefined') {
+        // If all of the modules dependencies are found then we apply it
+        modules[definition.name] = definition.fn.apply(Amber, definition.deps);
       } else {
+        // Otherwise we send it back to check later
         definitions.push(definitions[i]);
       }
+
+      // Set it to undefined so we don't store multiple copyies in memory
       definitions[i] = void 0;
 
+      // Ensure we don't hit an infinite loop if we can't resolve things
       if(i >= maxIterations) {
         throw new Error("Unable to resolve dependencies");
       }
     }
 
+    // optional Callback
     if(typeof callback === 'function') {
       Amber.inject(deps, callback);
     }
   };
+
+  // Resolve internal dependencies before any user defined deps
   Amber.run();
